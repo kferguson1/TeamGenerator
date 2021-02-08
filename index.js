18ae53a015e2b512d916198d14bc5199649f77dc
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2021-02-08 18:34:27 -0500

    Added prompts to AddManager

--- index.js
+++ index.js
@@ -12,6 +12,41 @@ const addEngineerCard = require('./src/engineercard');
 const addInternCard = require('./src/interncard');
 const addManagerCard = require('./src/managercard');
 const wrapcards = require('./src/cardwrap');
+const { type } = require("os");
 
 //Prompts
-const team = []
+const team = [];
+
+const addManager = [
+    {
+        name: 'role',
+        type: 'confirm',
+        message: 'Welcome, lets begin!',
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: 'Enter Manager Name:',
+    },
+    {
+        name: 'id',
+        type: 'input',
+        message: 'Please enter Employee ID',
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Please enter email address',
+    },
+    {
+        name: 'officenumber',
+        type: 'input',
+        message: 'Please enter Office Number',
+    },
+    {
+        name: 'next',
+        type: 'list',
+        choices: ['Add Engineer', 'Add Intern', 'No More Members to Add'],
+        message: 'Who would you like to add next?',
+    },
+];
